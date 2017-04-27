28910db927ca06d3eca765dd707434fc7b5d0b68
Matthew Mi <mzalive@gmail.com>
2017-04-27 13:11:35 +1000

    Update fetchClient
    Works fine with standalone version of fetchServer

--- src/FetchClient.java
+++ src/FetchClient.java
@@ -7,6 +7,10 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import net.sf.json.JSON;
 
 public class FetchClient {
 
@@ -14,7 +18,7 @@ public class FetchClient {
 	private static int port = 3780;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try(Socket socket = new Socket("sunrise.cis.unimelb.edu.au",port);){
+		try(Socket socket = new Socket("localhost",port);){
 			DataInputStream input = new DataInputStream(socket.getInputStream());
 			DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 			JSONObject newCommand = new JSONObject();
@@ -22,24 +26,29 @@ public class FetchClient {
 			resource.put("name", "Aaron");
 			resource.put("tags", "['photo','jpeg','jpg']");
 			resource.put("description", "Secret agent photo :-)");
-			resource.put("uri", "file:///usr/local/share/ezshare/photo.jpg");
+			resource.put("uri", "file:///Users/mzalive/GitHub/EZShare/resources/mountains.jpg");
 			resource.put("channel", "");
 			resource.put("owner", "*");
-			resource.put("ezserver", "sunrise.cis.unimelb.edu.au:3780");
+			resource.put("ezserver", "localhost:3780");
 			newCommand.put("command","FETCH");
 			newCommand.put("resourceTemplate", resource);
-			newCommand.put("command","FETCH");
 			System.out.println(newCommand.toJSONString());		
 			output.writeUTF(newCommand.toJSONString());
 			output.flush();	
 			System.out.println("Received from server:");
 			String result2 = input.readUTF();
-			String result3 = input.readUTF();
-			System.out.println(result2+result3);
-			File file = new File("/Users/leonchung/Desktop/aaron2.jpg");
+			System.out.println(result2);
+			result2 = input.readUTF();
+			System.out.println(result2);
+			result2 = input.readUTF();
+			System.out.println(result2);
+			File file = new File("/Users/mzalive/Desktop/test.jpg");
 			FileOutputStream fos = new FileOutputStream(file);
-			byte[] inputBytes = new byte[1743506];
+			JSONParser parser = new JSONParser();
+			int size =Integer.valueOf(((JSONObject)parser.parse(result2)).get("resourceSize").toString());
+			byte[] inputBytes = new byte[size];
 			int length =0;
+			System.out.println("Start Tramsmission");
 			while((length=input.read(inputBytes,0,inputBytes.length))>0){
 				fos.write(inputBytes,0,length);
 				fos.flush();
@@ -49,6 +58,9 @@ public class FetchClient {
 		catch(UnknownHostException e){
 			e.printStackTrace();
 		}catch (IOException e){	
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 	}
