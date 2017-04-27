a62656811669c90cd4f68b7947c00857c33f7d7c
congyiw <congyiw@student.unimelb.edu.au>
2017-04-27 10:53:57 +1000

    Add files via upload

---
+++ RemoveServer.java
@@ -0,0 +1,48 @@
+package Aaron1;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.json.simple.JSONObject;
+
+public class RemoveServer {
+	@SuppressWarnings("unused")
+	private ResourceManager resourceManager;
+	public RemoveServer(ResourceManager r) {
+		resourceManager = r;
+	}
+	@SuppressWarnings("unchecked")
+	public JSONObject remove(JSONObject clientCmd) {
+		JSONObject result = new JSONObject();
+		JSONObject resource = new JSONObject();
+
+		URI uri = null;
+		if (!clientCmd.containsKey("resource")) 
+			return returnErrorMsg("missing resource");
+		resource = (JSONObject) clientCmd.get("resource");
+		
+		if (resource.containsKey("uri"))
+			try {
+				uri = new URI(resource.get("uri").toString());
+				if (!uri.getScheme().equals("file"))
+					return returnErrorMsg("invalid resource");
+				File file = new File(uri);
+				if (!file.exists())
+					return returnErrorMsg("cannot remove resource");
+			} catch (URISyntaxException e) {
+				return returnErrorMsg("invalid resource");
+			}
+		@SuppressWarnings("unused")
+		Resource r = new Resource(uri.toString());		
+		result.put("response", "success");		
+		return null;	
+	}
+	@SuppressWarnings("unchecked")
+	private JSONObject returnErrorMsg(String msg) {
+		JSONObject result = new JSONObject();
+		result.put("response", "error");
+		result.put("errorMessage", msg);
+		return result;
+	}
+	
+}
