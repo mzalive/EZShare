c2c95ea911865a2bba459575574b40bfdf26ecec
congyiw <congyiw@student.unimelb.edu.au>
2017-04-27 17:31:46 +1000

    Add files via upload

--- src/ExchangeServer.java
+++ src/ExchangeServer.java
@@ -10,10 +10,13 @@ public class ExchangeServer {
 		resourceManager = r;
 	}
 	
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings({ "unchecked", "null" })
 	public  JSONObject exchange(JSONObject serverlist) {
 		JSONObject result = new JSONObject();
 		JSONObject serverrecord = new JSONObject();
+		ArrayList<JSONObject> ll = new ArrayList<JSONObject>();
+		
+		
 		
 		ArrayList<String> recordhostname = new ArrayList<String>();
 		ArrayList<String> recordport = new ArrayList<String>();
@@ -21,42 +24,44 @@ public class ExchangeServer {
 		String list1 = ""; 
 		String[] slist = null;
 		String[] slist1 = null;
+		
  	    if (serverlist == null) {
-			returnErrorMsg("missing or invalid server list");
-		}
-		if(serverlist.containsKey("hostname"))
-			list = serverlist.get("hostname").toString();
-		    slist = list.split("\r\n|  ");
-		    for(int i=0;i<slist.length;i++){
-				recordhostname.add(slist[i]);
-			}
-		    for(int a= 1;a<recordhostname.size();a=a+2)
-		    {
-		    	String n = recordhostname.get(a);
-		    	if(n.matches("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}")){
-		    		String s[] = n.split("\\.");  
-		            if(Integer.parseInt(s[0])<255)  
-		                if(Integer.parseInt(s[1])<255)  
-		                    if(Integer.parseInt(s[2])<255)  
-		                        if(Integer.parseInt(s[3])<255); 
-		    	}
-		    	returnErrorMsg("missing resourceTemplate"); 	
-		    }
-		if(serverlist.containsKey("port"))
-			list1 = serverlist.get("port").toString();
-		  slist1 = list1.split("\r\n|  ");
-		    for(int i=0;i<slist1.length;i++){
-				recordport.add(slist1[i]);
-			}
-		    for(int a= 1;a<recordhostname.size();a=a+2)
-		    {
-		    	String n = recordhostname.get(a);
-		    	int port = Integer.parseInt(n);
-               if (port<0 && port>65535)
-   		    	returnErrorMsg("missing resourceTemplate"); 	
-            	   
-		    }				
+			returnErrorMsg("missing or invalid server list"); 	    
+	    	if(serverlist.containsKey("hostname")){
+		    	list = serverlist.get("hostname").toString();
+		          slist = list.split("\r\n|  ");
+		            for(int i=0;i<slist.length;i++){
+				            recordhostname.add(slist[i]);
+			                               }
+		                     for(int a= 1;a<recordhostname.size();a=a+2)
+		                       {
+		    	                String n = recordhostname.get(a);
+		    	                     if(n.matches("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}")){
+		    		                    String s[] = n.split("\\.");  
+		                                if(Integer.parseInt(s[0])<255)  
+		                                  if(Integer.parseInt(s[1])<255)  
+		                                    if(Integer.parseInt(s[2])<255)  
+		                                      if(Integer.parseInt(s[3])<255); 
+		    	                              }
+		    	                      returnErrorMsg("missing resourceTemplate"); 	
+		                              }
+		             if(serverlist.containsKey("port"))
+			            list1 = serverlist.get("port").toString();
+		                 slist1 = list1.split("\r\n|  ");
+		                  for(int i=0;i<slist1.length;i++){
+				             recordport.add(slist1[i]);
+			                      }
+		                    for(int a= 1;a<recordhostname.size();a=a+2)
+		                     {
+		    	               String n = recordhostname.get(a);
+		    	                int port = Integer.parseInt(n);
+                                 if (port<0 && port>65535)
+   		    	                   returnErrorMsg("missing resourceTemplate"); 	    	   
+		                           }			} }   		    
 		result.put("response", "success");
+		ll.add(serverlist);	
+		serverrecord.put("serverlist",ll);
+		
 		return null;
 		
 		
