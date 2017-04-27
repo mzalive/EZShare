58fe3f3a75d69f74c08f04e90e4eee5a508f74ba
Matthew Mi <mzalive@gmail.com>
2017-04-27 13:09:49 +1000

    Update FetchServer
    implement server part to make it able to work alone

--- src/FetchServer.java
+++ src/FetchServer.java
@@ -1,34 +1,87 @@
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import javax.net.ServerSocketFactory;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
 
 public class FetchServer {
 	
-	private ResourceManager resourceManager;
+	private static int port = 3780;
+	private static int counter =0;
+	private static ResourceManager resourceManager; // resource manager handles all server resources
 	
 	
-	public FetchServer(ResourceManager r) {
-		resourceManager = r;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ServerSocketFactory factory = ServerSocketFactory.getDefault();
+		try(ServerSocket server = factory.createServerSocket(port)){
+			resourceManager = new ResourceManager(server.getInetAddress().toString(), Integer.toString(port));
+			System.out.println("Waiting for client connection.");
+			while(true){
+				Socket client = server.accept();
+				counter++;
+				System.out.println("Client "+counter+": "+"Applying for connection!");
+				Thread t = new Thread(()->serveClient(client));
+				t.start();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static void serveClient(Socket client){
+		try(Socket clientSocket = client){
+			JSONParser parser = new JSONParser();
+			DataInputStream input = new DataInputStream(clientSocket.getInputStream());
+			DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
+			//System.out.println("CLIENT: "+input.readUTF());
+			output.writeUTF("Server: Hi Client "+counter+" hah!!!");
+			JSONObject results = new JSONObject();
+			
+			//results.put("response", "success");
+			//output.writeUTF(results.toJSONString());
+			//Receive more data
+			while(true){
+				if(input.available()>0){
+					JSONObject command = (JSONObject)parser.parse(input.readUTF());
+		
+					System.out.println("COMMAND RECEIVED: " + command.toJSONString());
+					// JSONObject result1 = parseCommand(command);
+					fetch((JSONObject)command.get("resourceTemplate"), output);
+				}
+			}
+		}
+		catch(IOException | ParseException e){
+			e.printStackTrace();
+		}
 	}
 	
 	@SuppressWarnings("unchecked")
-	public void fetch(JSONObject resourceTemplate, DataOutputStream output) {
+	public static void fetch(JSONObject resourceTemplate, DataOutputStream output) {
 		JSONObject result = new JSONObject();
 		ArrayList<String> tags = new ArrayList<String>();
 		URI uri = null;
 	    String channel = "";
 	    
 		// validate resourceTemplate
+	    System.out.println("validate resourceTemplate");
 		if (resourceTemplate == null) {
 			returnErrorMsg(result, output, "missing resourceTemplate");
 			return;
@@ -36,6 +89,7 @@ public class FetchServer {
 		
 		// extract key from resourceTemplate
 		// only channel & uri relevant
+		System.out.println("extract key from resourceTemplate");
 		if (resourceTemplate.containsKey("channel"))
 			channel = resourceTemplate.get("channel").toString();
 		try {
@@ -54,10 +108,14 @@ public class FetchServer {
 		}
 		
 		// fetch resource
+		
+		System.out.println(resourceManager.getServerResources().get(0).toJSON().toJSONString());
+		System.out.println(uri.toString());
+		System.out.println("fetch resource");
 		Resource resource = resourceManager.getServerResource(channel, uri.toString());
-		File f = new File(uri);
+		File f = new File(uri.getPath());
 		if (resource == null || !f.exists()) {
-			// TODO no match. have no idea how to solve
+			System.out.println("no match resource");
 		} else try {
 			// respond
 			result.put("response", "success");
@@ -91,7 +149,7 @@ public class FetchServer {
 	
 	// private method for returning error msg
 	@SuppressWarnings("unchecked")
-	private void returnErrorMsg(JSONObject result, DataOutputStream output, String msg) {
+	private static void returnErrorMsg(JSONObject result, DataOutputStream output, String msg) {
 		result.put("response", "error");
 		result.put("errorMessage", msg);
 		try {
