922a6e2e0b5387bf16ce7308ac8c93d34af24f52
RockyLC <lc1826442304@sina.com>
2017-04-29 17:53:54 +1000

    Update with exchanging function

--- src/EZServer.java
+++ src/EZServer.java
@@ -33,13 +33,13 @@ public class EZServer {
 	private static String secret;
 	private static int counter =0;
 	private static ResourceManager resourceManager; // resource manager handles all server resources
-	
+
 	public static void main(String[] args) {
-		
+
 		// init logger
 		System.setProperty("java.util.logging.config.file", "src/logging.properties");
 		Logger logger = Logger.getLogger(EZServer.class.getName());
-		
+
 		// handle args
 		Options options = new Options();
 		options.addOption("advertisedhostname", true, "advertised hostname");
@@ -65,17 +65,17 @@ public class EZServer {
 			logger.setLevel(Level.ALL);
 			logger.info("debug mode on");
 		} else logger.setLevel(Level.OFF);
-		
+
 		if (cLine.hasOption("advertisedhostname")) {
 			hostname = cLine.getOptionValue("advertisedhostname");
 			logger.info("set advertised hostname : " + hostname);
 		} else logger.info("no assigned advertised hostname, using default : " + hostname);
-			
+
 		if (cLine.hasOption("connectionintervallimit")) {
 			connection_interval = Integer.valueOf(cLine.getOptionValue("connectionintervallimit"));
 			logger.info("set connection interval limit : " + connection_interval + "s");
 		} else logger.info("no assigned connection interval limit, using default : " + connection_interval + "s");
-		
+
 		if (cLine.hasOption("exchangeinterval")) {
 			exchange_interval = Integer.valueOf(cLine.getOptionValue("exchangeinterval"));
 			logger.info("set exchange interval limit : " + exchange_interval + "s");
@@ -93,10 +93,14 @@ public class EZServer {
 			secret = secretGen(32);
 			logger.info("no assigned secret, auto generate : " + secret);
 		}
-				
-		
+
+
 		ServerSocketFactory factory = ServerSocketFactory.getDefault();
 		try(ServerSocket server = factory.createServerSocket(port)){
+			// Create timer for periodical exchanging behavior
+			Timer timer = new Timer();
+			timer.schedule(new MyTask(resourceManager), 1000, 10000);
+
 			resourceManager = new ResourceManager(server.getInetAddress().toString(), Integer.toString(port));
 			logger.fine("Server established, standing by.");
 			while(true){
@@ -114,13 +118,13 @@ public class EZServer {
 	private static void serveClient(Socket client, int clientID){
 		Logger logger = Logger.getLogger(EZServer.class.getName());
 		String loggerPrefix = "Client " + clientID + ": ";
-		
+
 		JSONObject results = new JSONObject();
 		JSONParser parser = new JSONParser();
-		
+
 		JSONObject resourceTemplate = new JSONObject();
-		
-		
+
+
 		try(Socket clientSocket = client){
 			DataInputStream input = new DataInputStream(clientSocket.getInputStream());
 			DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
@@ -128,43 +132,43 @@ public class EZServer {
 			if(input.available() > 0){
 				JSONObject clientCommand = (JSONObject)parser.parse(input.readUTF());
 				logger.info(loggerPrefix + "original request: " + clientCommand.toJSONString());
-				
+
 				String command = clientCommand.get("command").toString();
 				logger.info(loggerPrefix + "command rcvd: " + command);
-				
+
 				switch (command) {
 				case "PUBLISH":
 					resourceTemplate = (JSONObject) clientCommand.get("resource");
 					ArrayList<JSONObject> outcomeJSON;
-					
+
 					// query object to handle publish command
 					PublishServer publishObject = new PublishServer(resourceTemplate, resourceManager);
 					outcomeJSON = publishObject.publish();
-					
+
 					// respond with the outcome of the operation
 					for (int i = 0; i < outcomeJSON.size(); i++) {
 						results.put("result"+i, outcomeJSON.get(i));
 					}
 					output.writeUTF(results.toJSONString());
 					break;
-					
+
 				case "REMOVE":
-					
+
 					break;
-					
+
 				case "SHARE":
-					
-					break;		
-					
+
+					break;
+
 				case "QUERY":
 					resourceTemplate = (JSONObject) clientCommand.get("resourceTemplate");
-					
+
 					// query server object to handle queries
 					QueryServer queryObject = new QueryServer(resourceManager);
 					ArrayList<JSONObject> resourcesJSONFormat;
-					
+
 					boolean relay = (boolean)clientCommand.get("relay");
-					
+
 					resourcesJSONFormat = queryObject.query(resourceTemplate);
 					JSONObject response = new JSONObject();
 					returnSuccessMsg(output);
@@ -173,20 +177,20 @@ public class EZServer {
 					results.put("resultSize", resourcesJSONFormat.size());
 					output.writeUTF(results.toJSONString());
 					break;
-					
+
 				case "FETCH":
 					fetch(clientCommand, output, clientID);
 					break;
 
 				case "EXCHANGE":
-					
+
 					break;
 				default:
 					logger.warning(loggerPrefix + "unknown command");
 					clientSocket.close();
 					break;
 				}
-				
+
 			}
 		}
 		}
@@ -194,19 +198,19 @@ public class EZServer {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
+
 	@SuppressWarnings("unchecked")
 	public static void fetch(JSONObject clientCommand, DataOutputStream output, int clientID) {
 		Logger logger = Logger.getLogger(EZServer.class.getName());
 		String loggerPrefix = "Client " + clientID + ": ";
-		
+
 		JSONObject resourceTemplate = new JSONObject();
 		JSONObject result = new JSONObject();
 		ArrayList<String> tags = new ArrayList<String>();
 		URI uri = null;
 	    String channel = "";
-	       
+
 		// validate resourceTemplate
 	    logger.info(loggerPrefix + "validating resourceTemplate");
 		if (clientCommand.get("resourceTemplate") == null) {
@@ -221,7 +225,7 @@ public class EZServer {
 		logger.info(loggerPrefix + "extracting key from resourceTemplate");
 		if (resourceTemplate.containsKey("channel"))
 			channel = resourceTemplate.get("channel").toString();
-		
+
 		try {
 			// TODO handle invalid uri
 			if (resourceTemplate.containsKey("uri")) {
@@ -238,9 +242,9 @@ public class EZServer {
 			logger.warning(loggerPrefix + "resourceTemplate does not exist, exit.");
 			return;
 		}
-		
+
 		// fetch resource
-		
+
 		logger.info(loggerPrefix + "fetching resource");
 		Resource resource = resourceManager.getServerResource(channel, uri.toString());
 		File f = new File(uri.getPath());
@@ -250,12 +254,12 @@ public class EZServer {
 			// respond
 			result.put("response", "success");
 			output.writeUTF(result.toJSONString());
-			
-			
+
+
 			JSONObject resourceJson = resource.toJSON();
 			resourceJson.put("resourceSize", f.length());
 			output.writeUTF(resourceJson.toJSONString());
-			
+
 			// Start transmission
 			logger.fine(loggerPrefix + "Start transmission");
 			RandomAccessFile byteFile = new RandomAccessFile(f, "r");
@@ -266,19 +270,19 @@ public class EZServer {
 			}
 			byteFile.close();
 			logger.fine(loggerPrefix + "Transmission finished");
-			
+
 			// resultSize
 			// TODO resultSize always be 1?
 			JSONObject resultSize = new JSONObject();
 			resultSize.put("resultSize", 1);
 			output.writeUTF(resultSize.toJSONString());
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-			
+
 	}
-	
+
 	/*
 	 *  method for returning error msg
 	 */
@@ -293,8 +297,8 @@ public class EZServer {
 			e.printStackTrace();
 		}
 		return;
-	}	
-	
+	}
+
 	/*
 	 *  method for returning success msg
 	 */
@@ -309,8 +313,8 @@ public class EZServer {
 		}
 		return;
 	}
-	
-		
+
+
 //	private static JSONObject parseCommand(JSONObject command){
 //		JSONObject results = new JSONObject();
 //        results.put("response", "success");
@@ -328,7 +332,7 @@ public class EZServer {
 //			results.put("REMOVE RESPONSE","success");
 //			return results;
 //		}
-//		
+//
 //		case "SHARE":{
 //			JSONObject resource = (JSONObject) command.get("resource");
 //            String secret =(String)command.get("secret");
@@ -342,23 +346,23 @@ public class EZServer {
 //			results.put("SHARE RESPONSE","success");
 //			return results;
 //		}
-//		
+//
 //		case "PUBLISH":{
 //			JSONObject resource = (JSONObject) command.get("resource");
 //			ArrayList<JSONObject> outcomeJSON;
-//			
+//
 //			// query object to handle publish command
 //			PublishServer publishObject = new PublishServer(resource, resourceManager);
 //			outcomeJSON = publishObject.publish();
-//			
+//
 //			// respond with the outcome of the operation
 //			for (int i = 0; i < outcomeJSON.size(); i++) {
 //				results.put("result"+i, outcomeJSON.get(i));
 //			}
-//			
+//
 //			return results;
 //		}
-//		
+//
 //		case "FETCH":{
 //			System.out.println("Fetching!");
 //			JSONObject resourceTemplate = (JSONObject) command.get("resourceTemplate");
@@ -372,48 +376,83 @@ public class EZServer {
 //			results.put("FETCH RESPONSE","success");
 //			return results;
 //		}
-//		
+//
 //		case "QUERY":{
 //			JSONObject resource = (JSONObject) command.get("resourceTemplate");
-//			
+//
 //			// query server object to handle queries
 //			QueryServer queryObject = new QueryServer(resourceManager);
 //			ArrayList<JSONObject> resourcesJSONFormat;
-//			
+//
 //			boolean relay = (boolean)command.get("relay");
-//			
+//
 //			resourcesJSONFormat = queryObject.query(resource);
-//			
+//
 //			for (int i = 0; i < resourcesJSONFormat.size(); i++) {
 //				results.put("result"+i, resourcesJSONFormat.get(i));
 //			}
-//			
+//
 //			return results;
-//		} 
-//		
+//		}
+//
 //		case "EXCHANGE":{
 //			break;
 //		}
-//		
+//
 //		default:			try{
 //			throw new Exception();
 //		}catch(Exception e){
 //			e.printStackTrace();
 //		}
 //		}
-//		return results;	
+//		return results;
 //	}
-	
+
 	/*
 	 * Large ramdon string generator
 	 * for generate server secret
 	 */
-	private static String secretGen(int length) {  
-	    StringBuilder builder = new StringBuilder(length);  
-	    for (int i = 0; i < length; i++) {  
-	        builder.append((char) (ThreadLocalRandom.current().nextInt(33, 128)));  
-	    }  
-	    return builder.toString();  
-	}  
-	
+	private static String secretGen(int length) {
+	    StringBuilder builder = new StringBuilder(length);
+	    for (int i = 0; i < length; i++) {
+	        builder.append((char) (ThreadLocalRandom.current().nextInt(33, 128)));
+	    }
+	    return builder.toString();
+	}
+
+}
+
+// constructor method
+	public MyTask(ResourceManager resourceManager) throws UnknownHostException, IOException{
+		//this.socket = socket;
+		this.resourceManager = resourceManager;
+		try(Socket socket = new Socket("localhost",3780);){
+			DataInputStream input = new DataInputStream(socket.getInputStream());
+			DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+		}
+	}
+    @Override
+    public void run() {
+    	// loop each serverresource in the resourceManager class and try to exchange it with current server record
+    	for(JSONObject j1 : resourceManager.serverlist){
+
+    		// get the hostname and port
+        String hostname = j1.get("hostname").toString();
+        int port = Integer.parseInt(j1.get("port").toString());
+
+        // Use exchanger class to exchange
+		Exchanger e = new Exchanger(hostname,port);
+		try {
+			e.exchange(resourceManager);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+    	}
+    	// print information after finished
+        System.out.println("Finished Exchanging!");
+
+
+    }
+
 }
